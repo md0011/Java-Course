d2e9c58e6801e03c2299a0a4644bddea62381751
Mayur Devle <mayurd0303@gmail.com>
2024-01-23 08:24:41 +0000

    Literals

---
+++ Literals/Literals.java
@@ -0,0 +1,28 @@
+/**
+ * Literals
+ */
+public class Literals {
+
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 56;
+        short age3 = 87;
+        long ageDino = 5666666666666L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66;
+        boolean a = true;
+        String str = "Mayur Dev";
+
+        System.out.println("Byte: " + age);
+        System.out.println("Integer: " + age2);
+        System.out.println("Short: " + age3);
+        System.out.println("Long: " + ageDino);
+        System.out.println("Character: " + ch);
+        System.out.println("Float: " + f1);
+        System.out.println("Double: " + d1);
+        System.out.println("Boolean: " + a);
+        System.out.println("String: " + str);
+
+    }
+}
