5ac445840f2b1c65a14c97393de094c0bb999d59
Mayur Devle <mayurd0303@gmail.com>
2024-01-24 09:19:11 +0000

    Getting User Input

--- Getting User Input/UserInput.java
+++ Getting User Input/UserInput.java
@@ -1,22 +1,22 @@
-import java.util.Scanner;  // Importing  the Scanner class
+import java.util.Scanner; // Importing  the Scanner class
 
 public class UserInput {
 
     public static void main(String[] args) {
         System.out.println("Taking Input From the User");
         Scanner sc = new Scanner(System.in);
-       System.out.println("Enter number 1");
-       int a = sc.nextInt();
-       System.out.println("Enter number 2");
-       int b = sc.nextInt();
-//    float a = sc.nextFloat();
-//    float b = sc.nextFloat();
+        System.out.println("Enter number 1");
+        int a = sc.nextInt();
+        System.out.println("Enter number 2");
+        int b = sc.nextInt();
+        // float a = sc.nextFloat();
+        // float b = sc.nextFloat();
 
-       int sum = a + b;
-       System.out.println("The sum of these numbers is");
-       System.out.println(sum);
-//        boolean b1 = sc.hasNextInt();
-//        System.out.println(b1);
+        int sum = a + b;
+        System.out.println("The sum of these numbers is");
+        System.out.println(sum);
+        // boolean b1 = sc.hasNextInt();
+        // System.out.println(b1);
         // String str = sc.next();
         String str = sc.nextLine();
         System.out.println(str);
@@ -24,5 +24,5 @@ public class UserInput {
         sc.close();
 
     }
-    
+
 }
