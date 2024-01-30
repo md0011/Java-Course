448e9e7dba8ab4ce045888fe68a011374a8717b8
Mayur Devle <mayurd0303@gmail.com>
2024-01-30 10:58:12 +0000

    If-Else Statement

---
+++ If-else Statement/IfElseStatement.java
@@ -0,0 +1,42 @@
+/**
+ * IfElseStatement
+ */
+public class IfElseStatement {
+
+    public static void main(String[] args) {
+        /*
+         * if (condition-to-be-checked) {
+         * statements-if-condition-true;
+         * }
+         * else {
+         * statements-if-condition-false;
+         * }
+         */
+
+        int a = 29;
+        if (a > 18) {
+            System.out.println("You can drive");
+        } else {
+            System.out.println("You are underage!");
+        }
+
+        /*
+         * if (condition1) {
+         * 
+         * //Statements;
+         * 
+         * else if {
+         * 
+         * // Statements;
+         * 
+         * }
+         * 
+         * else {
+         * 
+         * //Statements
+         * 
+         * }
+         */
+
+    }
+}
