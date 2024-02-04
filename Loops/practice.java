62d3a0a3fb156e13729b2ea2a290a1606c3e92a9
Mayur Devle <mayurd0303@gmail.com>
2024-02-04 05:49:06 +0000

    print pattern

---
+++ Loops/practice.java
@@ -0,0 +1,20 @@
+package Loops;
+
+/**
+ * practice
+ */
+public class practice {
+
+    public static void main(String[] args) {
+        
+    // Practice Problem 1
+       int n = 4;
+       for (int i=n; i>0; i--){
+           for(int j=0;j<i;j++){
+               System.out.print("*");
+           }
+           System.out.print("\n");
+       }
+       
+    }
+}
