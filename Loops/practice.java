7a9859cfc6379204e65779750231d5b41e35893a
Mayur Devle <mayurd0303@gmail.com>
2024-02-05 07:24:58 +0000

    Sum of first n even numbers

--- Loops/practice.java
+++ Loops/practice.java
@@ -6,15 +6,36 @@ package Loops;
 public class practice {
 
     public static void main(String[] args) {
-        
-    // Practice Problem 1
-       int n = 4;
-       for (int i=n; i>0; i--){
-           for(int j=0;j<i;j++){
-               System.out.print("*");
-           }
-           System.out.print("\n");
-       }
-       
+
+        // Question 1: Write a program to print the following pattern :
+        // ****
+        // ***
+        // **
+        // *
+
+        System.out.println("Question 1: Write a program to print the pattern:-");
+
+        int k = 4;
+        for (int i = k; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+
+        System.out.println("--------------------------------------------------------");
+
+        System.out.println("Question 2: Write a program to sum first n even numbers:-");
+        int sum = 0;
+        int n = 4;
+        for (int i = 0; i < n; i++) {
+            sum = sum + (2 * i);
+        }
+        System.out.print("Sum of even numbers is: ");
+        System.out.println(sum);
+        // First 4 even numbers are - 0 2 4 6
+
+        System.out.println("--------------------------------------------------------");
+
     }
 }
