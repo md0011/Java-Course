e7041110c18715a65cf3933c99f0af06092d206a
Mayur Devle <mayurd0303@gmail.com>
2024-02-05 07:36:50 +0000

    Multiplication table in reverse order

--- Loops/practice.java
+++ Loops/practice.java
@@ -7,13 +7,13 @@ public class practice {
 
     public static void main(String[] args) {
 
-        // Question 1: Write a program to print the following pattern :
+        System.out.println("----------------------------------------------------------------------------------------------------------------");
+        
+        System.out.println("Question 1: Write a program to print the pattern:-");
         // ****
         // ***
         // **
         // *
-
-        System.out.println("Question 1: Write a program to print the pattern:-");
 
         int k = 4;
         for (int i = k; i > 0; i--) {
@@ -23,7 +23,7 @@ public class practice {
             System.out.print("\n");
         }
 
-        System.out.println("--------------------------------------------------------");
+        System.out.println("----------------------------------------------------------------------------------------------------------------");
 
         System.out.println("Question 2: Write a program to sum first n even numbers:-");
         int sum = 0;
@@ -35,7 +35,7 @@ public class practice {
         System.out.println(sum);
         // First 4 even numbers are - 0 2 4 6
 
-        System.out.println("--------------------------------------------------------");
+        System.out.println("----------------------------------------------------------------------------------------------------------------");
 
         System.out.println("Question 3: Write a program to print the multiplication table of a given number n:");
         int a = 5;
@@ -44,7 +44,16 @@ public class practice {
                    System.out.printf("%d X %d = %d\n", a, i, a*i);
                }
 
-        System.out.println("--------------------------------------------------------");
+        System.out.println("----------------------------------------------------------------------------------------------------------------");
 
+        System.out.println("Question 4: Write a program to print a multiplication table of 10 in reverse order:");
+        int b = 10;
+               //for(int i=0; i<10; i++) - Goes from i=0 to i=9
+               for(int i=10;i>=1;i--){
+                   System.out.printf("%d X %d = %d\n", b, i, b*i);
+               }
+
+        System.out.println("----------------------------------------------------------------------------------------------------------------");
+        
     }
 }
