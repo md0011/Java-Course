4f9219f8dd91fc22267be9afa85d9e2873237013
Mayur Devle <mayurd0303@gmail.com>
2024-02-10 07:08:33 +0000

    Given integer is present in an array or not

--- Arrays/Practice.java
+++ Arrays/Practice.java
@@ -16,8 +16,26 @@ public class Practice {
             sum = sum + element;
         }
         System.out.println("The value of sum is " + sum);
-        
-        System.out.println("-----------------------------------------------------------------------------");
+
+        System.out.println("-----------------------------------------------------------------------------------------------------");
+
+        System.out.println("Question 2. Write a program to find out whether a given integer is present in an array or not:");
+        float[] marks1 = { 45.7f, 67.8f, 63.4f, 99.2f, 100.0f };
+        float num = 45.75f;
+        boolean isInArray = false;
+        for (float element1 : marks1) {
+            if (num == element1) {
+                isInArray = true;
+                break;
+            }
+        }
+        if (isInArray) {
+            System.out.println("The value is present in the array!");
+        } else {
+            System.out.println("The value is not present in the array!");
+        }
+
+        System.out.println("-----------------------------------------------------------------------------------------------------");
 
     }
 }
