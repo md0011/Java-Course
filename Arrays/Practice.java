b9f650951781e5fb5399ab1afb3e27f467ac1d5f
Mayur Devle <mayurd0303@gmail.com>
2024-02-12 07:03:42 +0000

    Reverse an array

--- Arrays/Practice.java
+++ Arrays/Practice.java
@@ -72,5 +72,24 @@ public class Practice {
 
         System.out.println("---------------------------------------------------------------------------------------------------------------------------");
 
+        System.out.println("Question 5. Write a Java program to reverse an array:");
+        int [] arr = {1, 21, 3, 4, 5, 34, 67};
+        int l = arr.length;
+        int n = Math.floorDiv(l, 2);
+        int temp;
+        for(int i=0; i<n; i++){
+            // Swap a[i] and a[l-1-i]
+            // a   b   temp
+            // |4| |3| ||
+            temp = arr[i];
+            arr[i] = arr[l-i-1];
+            arr[l-i-1] = temp;
+        }
+        for(int element: arr){
+            System.out.print(element + " ");
+        }
+        System.out.println();
+        System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+
     }
 }
