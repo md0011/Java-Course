fa87a43e5e2b1e75a208bab9a8eec19bf6ff2985
Mayur Devle <mayurd0303@gmail.com>
2024-02-13 06:21:50 +0000

    An array is sorted or not

--- Arrays/Practice.java
+++ Arrays/Practice.java
@@ -6,20 +6,23 @@ package Arrays;
 public class Practice {
 
     public static void main(String[] args) {
-        
-        System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+
+        System.out.println(
+                "---------------------------------------------------------------------------------------------------------------------------");
 
         System.out.println("Question 1. Create an array of 5 floats and calculate their sum:");
-        float [] marks = {45.7f, 67.8f, 63.4f, 99.2f, 100.0f};
+        float[] marks = { 45.7f, 67.8f, 63.4f, 99.2f, 100.0f };
         float sum = 0;
-        for(float element:marks){
+        for (float element : marks) {
             sum = sum + element;
         }
         System.out.println("The value of sum is " + sum);
 
-        System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+        System.out.println(
+                "---------------------------------------------------------------------------------------------------------------------------");
 
-        System.out.println("Question 2. Write a program to find out whether a given integer is present in an array or not:");
+        System.out.println(
+                "Question 2. Write a program to find out whether a given integer is present in an array or not:");
         float[] marks1 = { 45.7f, 67.8f, 63.4f, 99.2f, 100.0f };
         float num = 45.75f;
         boolean isInArray = false;
@@ -35,73 +38,97 @@ public class Practice {
             System.out.println("The value is not present in the array!");
         }
 
-        System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+        System.out.println(
+                "---------------------------------------------------------------------------------------------------------------------------");
 
-        System.out.println("Question 3. Calculate the average marks from an array containing marks of all students in physics using a for-each loop:");
-        float [] marks2 = {45.7f, 67.8f, 63.4f, 99.2f, 100.0f};
+        System.out.println(
+                "Question 3. Calculate the average marks from an array containing marks of all students in physics using a for-each loop:");
+        float[] marks2 = { 45.7f, 67.8f, 63.4f, 99.2f, 100.0f };
         float sum2 = 0;
-        for(float element2:marks2){
+        for (float element2 : marks2) {
             sum2 = sum2 + element2;
         }
-        System.out.println("The value of average marks is " + sum2/marks2.length);
+        System.out.println("The value of average marks is " + sum2 / marks2.length);
 
-        System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+        System.out.println(
+                "---------------------------------------------------------------------------------------------------------------------------");
 
         System.out.println("Question 4. Create a Java program to add two matrices of size 2x3:");
-        int [][] mat1 = {{1, 2, 3},
-                         {4, 5, 6}};
-        int [][] mat2 = {{2, 6, 13},
-                         {3, 7, 1}};
-        int [][] result = {{0, 0, 0},
-                           {0, 0, 0}};
+        int[][] mat1 = { { 1, 2, 3 },
+                { 4, 5, 6 } };
+        int[][] mat2 = { { 2, 6, 13 },
+                { 3, 7, 1 } };
+        int[][] result = { { 0, 0, 0 },
+                { 0, 0, 0 } };
 
-        for (int i=0;i<mat1.length;i++){ // row number of times
-            for (int j=0;j<mat1[i].length;j++) { // column number of time
+        for (int i = 0; i < mat1.length; i++) { // row number of times
+            for (int j = 0; j < mat1[i].length; j++) { // column number of time
                 System.out.format(" Setting value for i=%d and j=%d\n", i, j);
                 result[i][j] = mat1[i][j] + mat2[i][j];
             }
         }
         // Printing the elements of a 2-D Array
-        for (int i=0;i<mat1.length;i++){ // row number of times
-            for (int j=0;j<mat1[i].length;j++) { // column number of time
+        for (int i = 0; i < mat1.length; i++) { // row number of times
+            for (int j = 0; j < mat1[i].length; j++) { // column number of time
                 System.out.print(result[i][j] + " ");
                 result[i][j] = mat1[i][j] + mat2[i][j];
             }
             System.out.println(""); // Prints a new line
         }
 
-        System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+        System.out.println(
+                "---------------------------------------------------------------------------------------------------------------------------");
 
         System.out.println("Question 5. Write a Java program to reverse an array:");
-        int [] arr = {1, 21, 3, 4, 5, 34, 67};
+        int[] arr = { 1, 21, 3, 4, 5, 34, 67 };
         int l = arr.length;
         int n = Math.floorDiv(l, 2);
         int temp;
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             // Swap a[i] and a[l-1-i]
-            // a   b   temp
+            // a b temp
             // |4| |3| ||
             temp = arr[i];
-            arr[i] = arr[l-i-1];
-            arr[l-i-1] = temp;
+            arr[i] = arr[l - i - 1];
+            arr[l - i - 1] = temp;
         }
-        for(int element: arr){
+        for (int element : arr) {
             System.out.print(element + " ");
         }
         System.out.println();
-        System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+        System.out.println(
+                "---------------------------------------------------------------------------------------------------------------------------");
 
         System.out.println("Question 6. Write a Java program to find the maximum element in an array:");
-        int [] arr1 = {1, 2100, 3, 455, 5, 34, 67};
+        int[] arr1 = { 1, 2100, 3, 455, 5, 34, 67 };
         int max = Integer.MIN_VALUE;
-        for(int e: arr1){
-            if(e>max){
+        for (int e : arr1) {
+            if (e > max) {
                 max = e;
             }
         }
-        System.out.println("The value of the maximum element in this array is: "+ max);
+        System.out.println("The value of the maximum element in this array is: " + max);
 
-        System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+        System.out.println(
+                "---------------------------------------------------------------------------------------------------------------------------");
+
+        System.out.println("Question 7. Write a Java program to find whether an array is sorted or not:");
+        boolean isSorted = true;
+        int[] arr2 = { 1, 12, 3, 4, 5, 34, 67 };
+        for (int i = 0; i < arr2.length - 1; i++) {
+            if (arr2[i] > arr2[i + 1]) {
+                isSorted = false;
+                break;
+            }
+        }
+        if (isSorted) {
+            System.out.println("The Array is sorted");
+        } else {
+            System.out.println("The Array is not sorted");
+        }
+
+        System.out.println(
+                "---------------------------------------------------------------------------------------------------------------------------");
 
     }
 }
