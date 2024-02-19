05b860d7232945a56d9c6fab7305e4fc36c24d53
Mayur Devle <mayurd0303@gmail.com>
2024-02-19 06:24:45 +0000

    Print the pattern

--- Exercise 3/PrintPattern.java
+++ Exercise 3/PrintPattern.java
@@ -3,6 +3,11 @@
  */
 public class PrintPattern {
 
+    // Write a program using functions to print the following pattern:
+    // *
+    // **
+    // ***
+    // ****
     static void pattern1(int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i + 1; j++) {
@@ -12,15 +17,27 @@ public class PrintPattern {
         }
     }
 
+    // Write a program using functions to print the following pattern:
+    // ****
+    // ***
+    // **
+    // *
+    public static void pattern2(int rows) {
+        for (int x = rows; x >= 1; x--) {
+            for (int y = 1; y <= x; y++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 
-        // Write a program using functions to print the following pattern:
-        // *
-        // **
-        // ***
-        // ****
+        pattern1(4);
 
-        pattern1(10);
+        System.out.println("-------------------------------------");
+
+        pattern2(4);
 
     }
 }
