52baab011befd6235f7df341a737db4dc7383720
Mayur Devle <mayurd0303@gmail.com>
2024-02-19 06:14:51 +0000

    Recursive Function

---
+++ Exercise 3/RecursiveFunction.java
@@ -0,0 +1,24 @@
+public class RecursiveFunction {
+
+    // sum(n) = 1 + 2 + 3... + n
+    // sum(n) = 1 + 2 + 3... + n-1 + n
+    // sum(n) = sum(n-1) + n
+    // sum(3) = 3 + sum(2)
+    // sum(3) = 3 + 2 + sum(1)
+    // sum(3) = 3 + 2 + 1
+    static int sumRec(int n) {
+        // Base condition
+        if (n == 1) {
+            return 1;
+        }
+        return n + sumRec(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        // Write a recursive function to calculate the sum of first n natural numbers.
+        int c = sumRec(4);
+        System.out.println(c);
+
+    }
+}
